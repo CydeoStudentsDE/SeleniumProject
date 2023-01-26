21e56add09be0242886316f85dbb7de458ad7601
Ömer <lgoh597@gmail.com>
2023-01-26 14:39:14 +0100

    deneme

--- src/test/java/com/cydeo/tests/Omer/Day02_Locators_FindElement_GetText_GetAttribute/HWP1_Etsy.java
+++ src/test/java/com/cydeo/tests/Omer/Day02_Locators_FindElement_GetText_GetAttribute/HWP1_Etsy.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HWP1_Etsy {
     public static void main(String[] args) throws InterruptedException {
+
         //HWP #1: Etsy Title Verification
         //1. Open Chrome browser
         WebDriverManager.chromedriver().setup();
